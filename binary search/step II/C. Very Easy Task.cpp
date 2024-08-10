148465223577526c2c1c8dd5f0239da360497081
Gabriel-Andrade-Z <gabrielasantana1602@gmail.com>
2024-08-09 22:14:33 -0300

    update

---
+++ binary search/step II/C. Very Easy Task.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+ll n, x, y;
+
+bool check(ll m){
+	if(m < min(x,y)) return false;
+	m -= min(x,y);
+	return (m / x) + (m / y) + 1 >= n;
+}
+
+void solve(){
+  	cin >> n >> x >> y;
+  	ll l = 0, r = max(x,y)*n;
+ 	while(r > l + 1){
+    		ll m = (r + l) >> 1;
+    		if(check(m)) r = m;
+    		else l = m;
+  	}
+  	cout << r << '\n';
+}
+
+int main() {
+  	cout.tie(0); cin.tie(0);
+	cout.sync_with_stdio(0);
+  	solve();
+  	return 0;
+}
